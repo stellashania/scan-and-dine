28bdb42bb90dc87bbdfdedede138bae99478f7e1
Juanrico Alvaro <juanricoalvaro@gmail.com>
2021-11-03 15:47:46 +0700

    create migration menu, category, cart item

---
+++ database/migrations/2021_11_03_083231_create_menus_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMenusTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('menus', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('category_id');
+            $table->foreign('category_id')->references('id')->on('categories')->onUpdate('cascade')->onDelete('cascade');
+            $table->string('name');
+            $table->string('description');
+            $table->integer('price');
+            $table->string('image');
+            $table->boolean('active');
+            $table->timestamps();            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('menus');
+    }
+}

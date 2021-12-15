aeb89cb24827ed50f470487a5d05a1cbe1ff4d3f
David Samuel <davidsamuel.7878@gmail.com>
2021-12-15 16:41:11 +0700

    updates

--- database/seeders/CartItemSeeder.php
+++ database/seeders/CartItemSeeder.php
@@ -14,17 +14,37 @@ class CartItemSeeder extends Seeder
      */
     public function run()
     {
-        //
-        // CartItem::create([
-        //     'cart_id' => 1,
-        //     'menu_id' => 1,
-        //     'quantity' => 2
-        // ]);
+        
+        CartItem::create([
+            'cart_id' => 1,
+            'menu_id' => 1,
+            'quantity' => 2
+        ]);
 
-        // CartItem::create([
-        //     'cart_id' => 1,
-        //     'menu_id' => 1,
-        //     'quantity' => 5
-        // ]);
+        CartItem::create([
+            'cart_id' => 1,
+            'menu_id' => 1,
+            'quantity' => 5
+        ]);
+
+        CartItem::create([
+            'cart_id' => 1,
+            'menu_id' => 1,
+            'quantity' => 5
+        ]);
+
+        CartItem::create([
+            'cart_id' => 1,
+            'menu_id' => 1,
+            'quantity' => 5
+        ]);
+
+        CartItem::create([
+            'cart_id' => 1,
+            'menu_id' => 1,
+            'quantity' => 5
+        ]);
+
+        
     }
 }
